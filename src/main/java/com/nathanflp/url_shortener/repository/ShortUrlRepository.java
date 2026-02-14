f6bd171219b00a34efe40bd2bbd0ce21b8797abd
Nathan Feliphe <nathanfcrocha@gmail.com>
2026-02-14 13:56:17 -0300

    feat: add business logic to service and repository

--- src/main/java/com/nathanflp/url_shortener/repository/ShortUrlRepository.java
+++ src/main/java/com/nathanflp/url_shortener/repository/ShortUrlRepository.java
@@ -1,7 +1,11 @@
 package com.nathanflp.url_shortener.repository;
 
-import com.nathanflp.url_shortener.models.*;
+import com.nathanflp.url_shortener.domain.model.*;
 import org.springframework.data.jpa.repository.*;
 
+import java.util.*;
+
 public interface ShortUrlRepository extends JpaRepository<ShortUrl,Integer> {
+    Optional<ShortUrl> findById(String shortenUrlId);
+    Optional<ShortUrl> findByOriginalUrl(String shortenUrlId);
 }
