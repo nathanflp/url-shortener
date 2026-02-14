e8fa520e368b98e9d14abb6825dba52fdc1cb66b
Nathan Feliphe <nathanfcrocha@gmail.com>
2026-02-14 13:37:26 -0300

    feat: refactor of directories and create ShortUrl entity,dto's and factory

---
+++ src/main/java/com/nathanflp/url_shortener/dto/factory/ShortUrlDTOFactory.java
@@ -0,0 +1,26 @@
+package com.nathanflp.url_shortener.dto.factory;
+
+import com.nathanflp.url_shortener.domain.model.*;
+import com.nathanflp.url_shortener.dto.response.*;
+import com.nathanflp.url_shortener.utils.*;
+
+public class ShortUrlDTOFactory {
+
+    public static ShortUrlResponse toResponse (String fullShortenUrl, ShortUrl shortUrl){
+        return new ShortUrlResponse(fullShortenUrl,
+                DateFormatter.formatInstant(shortUrl.getExpiresAt())
+        );
+    }
+
+
+    public static ShortUrlMetricsResponse toMetricsResponse(ShortUrl shortUrlEntity) {
+        return new ShortUrlMetricsResponse(
+                shortUrlEntity.getId(),
+                shortUrlEntity.getOriginalUrl(),
+                shortUrlEntity.getClickCount(),
+                DateFormatter.formatInstant(shortUrlEntity.getCreatedAt()),
+                DateFormatter.formatInstant(shortUrlEntity.getLastTimeClicked()),
+                DateFormatter.formatInstant(shortUrlEntity.getExpiresAt())
+        );
+    }
+}
