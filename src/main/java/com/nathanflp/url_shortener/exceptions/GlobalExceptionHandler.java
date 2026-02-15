22b7bed4975297b7504d8a4de43c905fb372226b
Nathan Feliphe <nathanfcrocha@gmail.com>
2026-02-15 17:58:40 -0300

    feat: update GlobalExceptionHandler annotation

--- src/main/java/com/nathanflp/url_shortener/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/nathanflp/url_shortener/exceptions/GlobalExceptionHandler.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.mvc.method.annotation.*;
 
 import java.time.*;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(UrlNotFoundException.class)
