aacda8eb66ba24777de4764796432ad2f7dd54e3
Nathan Feliphe <nathanfcrocha@gmail.com>
2026-07-27 19:10:55 -0300

    test: add H2 database dependency and test profile configuration

--- src/test/java/com/nathanflp/url_shortener/UrlShortenerApplicationTests.java
+++ src/test/java/com/nathanflp/url_shortener/UrlShortenerApplicationTests.java
@@ -2,8 +2,10 @@ package com.nathanflp.url_shortener;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UrlShortenerApplicationTests {
 
 	@Test
